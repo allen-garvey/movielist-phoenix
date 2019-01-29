21dcb70fefbf43d93062f8ad4bc867f2b4d76f62
Allen Garvey <allen.garvey@gmail.com>
2019-01-29 16:54:30 -0500

    Enable sass compilation

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,9 +29,18 @@ module.exports = (env, options) => ({
         }
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
+        test: /\.scss$/,
+        use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                {
+                  loader: 'sass-loader',
+                  options: {
+                      outputStyle: 'compressed',
+                  },
+                },
+              ]
+      },
     ]
   },
   plugins: [
