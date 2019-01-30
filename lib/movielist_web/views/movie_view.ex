2754672921b8d3b72763cd64b929bd3a112f4437
Allen Garvey <allen.garvey@gmail.com>
2019-01-30 13:47:25 -0500

    Add to_s and map for form functions to views

--- lib/movielist_web/views/movie_view.ex
+++ lib/movielist_web/views/movie_view.ex
@@ -1,3 +1,14 @@
 defmodule MovielistWeb.MovieView do
   use MovielistWeb, :view
+
+  def to_s(movie) do
+  	movie.title
+  end
+
+  @doc """
+  Maps a list of movies into tuples, used for forms
+  """
+  def map_for_form(movies) do
+    Enum.map(movies, &{to_s(&1), &1.id})
+  end
 end
--- lib/movielist_web/views/rating_view.ex
+++ lib/movielist_web/views/rating_view.ex
@@ -1,3 +1,12 @@
 defmodule MovielistWeb.RatingView do
   use MovielistWeb, :view
+
+  alias MovielistWeb.MovieView
+  alias MovielistWeb.SharedView
+
+  def to_s(rating) do
+  	MovieView.to_s(rating.movie) <> "—" <> SharedView.us_formatted_date(rating.date_scored)
+  end
+
+
 end
