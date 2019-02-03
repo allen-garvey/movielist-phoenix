afa1648da51811b34c731bae2c2c82283c2ab2f2
Allen Garvey <allen.garvey@gmail.com>
2019-02-03 15:08:05 -0500

    Change index page to redirect to active movies index page

--- lib/movielist_web/controllers/page_controller.ex
+++ lib/movielist_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule MovielistWeb.PageController do
   use MovielistWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> redirect(to: Routes.movie_path(conn, :index_active))
   end
 end
