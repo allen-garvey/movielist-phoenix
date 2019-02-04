dbbe75a6e1c45aab6433e5cab729ca8f035fd027
Allen Garvey <allen.garvey@gmail.com>
2019-02-04 09:10:02 -0500

    Add helper function to round decimals

--- lib/movielist_web/views/shared_view.ex
+++ lib/movielist_web/views/shared_view.ex
@@ -28,6 +28,15 @@ defmodule MovielistWeb.SharedView do
 	end
 
 	@doc """
+	Rounds decimal
+	"""
+	def round_decimal(decimal) do
+		decimal
+		  |> Decimal.to_float 
+		  |> Float.round(2)
+	end
+
+	@doc """
   	Creates a form group container for a label and input
   	"""
 	def form_group(field, form, fun) when is_atom(field) and is_function(fun, 3) do
